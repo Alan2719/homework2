3a37938775d3262ab0e634578dffa39e4023f1c4
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-07-27 21:33:24 -0500

    changed portfolio, contact

--- portfolio.js
+++ portfolio.js
@@ -7,6 +7,9 @@ function hideAll() {
     $(".note-taker").hide();
     $(".readme").hide();
     $(".team-generator").hide();
+    $(".employeesDB").hide();
+    $(".burger").hide();
+    $(".handshake").hide();
 }
 
 $("#password-generator").on('click',function(event){
@@ -80,6 +83,48 @@ $("#team-generator").on('click',function(event){
     $(".team-generator").css('display','block');
     $("#link").attr('href','https://github.com/Alan2719/ReadmeGenerator');
 });
+
+$("#employeesDB").on('click',function(event){
+    event.preventDefault();
+    hideAll();
+    console.log($(this));
+    $(".display").css('display','block');
+    $(".employeesDB").css('display','block');
+    $("#link").attr('href','https://github.com/Alan2719/EmployeeTracker');
+});
+
+$("#burger").on('click',function(event){
+    event.preventDefault();
+    hideAll();
+    console.log($(this));
+    $(".display").css('display','block');
+    $(".burger").css('display','block');
+    $("#link").attr('href','https://github.com/Alan2719/eat-da-burger');
+});
+
+$("#handshake").on('click',function(event){
+    event.preventDefault();
+    hideAll();
+    console.log($(this));
+    $(".display").css('display','block');
+    $(".handshake").css('display','block');
+    $("#link").attr('href','https://handshake-service-app.herokuapp.com/');
+});
+
+$("#show-more").on('click',function(){
+    console.log("hola");
+    $(".no-display").css('display','block');
+    $("#btn-cont").css('display','none');
+    $(".show-less").css('display','block');
+});
+
+$("#show-less").on('click',function(){
+    console.log("hola");
+    $(".no-display").css('display','none');
+    $("#btn-cont").css('display','display');
+    $(".show-more").css('display','block');
+    $(".show-less").css('display','none');
+})
 
 $("#close").on('click',function(){
    hideAll();
